2644a24af5307580f2ea42538917064f53a9899a
ebkendall <ebkendal@ncsu.edu>
2024-09-16 09:23:26 -0400

    updated test runfile

--- VAR_three_meds/five_states/test_runfile.r
+++ VAR_three_meds/five_states/test_runfile.r
@@ -50,33 +50,30 @@ for(i in EIDs){
 }
 
 # -----------------------------------------------------------------------------
-# Isolate one subject and test efficacy of state sampler and see:
-# (a) actual posterior distribution of states
-# (b) compare to distribution of state proposals
-# (c) compare to "stationary" distribution (marginal probability of states at each time)
-# Also, write a sanity check code to ensure that the proposal probabilities are what we expect
-# Then, dig into MH ratio and see how we can incorporate the response values
-# -----------------------------------------------------------------------------
-# i = 502250
-# ii = which(EIDs == i)
-# EIDs_temp = i
-# par_temp = true_pars
-# Y_temp = Y[Y[,"EID"] == i, ]
-# z_temp = z[Y[,"EID"] == i, ]
-# n_i = nrow(Y_temp)
-# b_i_true = data_format[data_format[,"EID"] == i,"b_true"]
-
-# -----------------------------------------------------------------------------
 # Focusing on a few subjects --------------------------------------------------
 # -----------------------------------------------------------------------------
-EIDs_temp = c(194350, 234375, 259825, 288775, 747775)
+# EID_group = vector(mode = 'list', length = 100)
+# EID_group[[1]] = c(194350, 234375, 259825, 288775, 747775)
+# EID_ordering = sample(EIDs[-which(EIDs %in% c(194350, 234375, 259825, 288775, 747775))], 
+#                       size = 495, replace = F)
+# for(e in 2:length(EID_group)) {
+#     EID_group[[e]] = EID_ordering[(5*(e-1)+1):(5*(e-1)+5)]
+# }
+# save(EID_group, file = 'Data_sim/EID_group.rda')
+load('Data_sim/EID_group.rda')
+
+args = commandArgs(TRUE)
+e_num = as.numeric(args[1])
+print(paste0("EID group: ", e_num))
+
+EIDs_temp = EID_group[[e_num]]
 par_temp = true_pars
 ii_s = which(EIDs %in% EIDs_temp)
 Y_temp_big = Y[Y[,"EID"] %in% EIDs_temp, ]
 z_temp_big = z[Y[,"EID"] %in% EIDs_temp, ]
 b_i_true = data_format[data_format[,"EID"] %in% EIDs_temp, "b_true"]
 # Initialize a state sequence -------------------------------------------------
-set.seed(1)
+set.seed(e_num)
 for(i in EIDs_temp) {
     Y_temp = Y_temp_big[Y_temp_big[,"EID"] == i, ]
     z_temp = z_temp_big[Y_temp_big[,"EID"] == i, ]
@@ -129,25 +126,9 @@ for(i in EIDs_temp) {
     B[[which(EIDs == i)]] = matrix(b_i, ncol = 1)
 }
 
-
 Dn_Xn = update_Dn_Xn_cpp( as.numeric(EIDs), B, Y, true_pars, par_index, x, 10)
 Dn = Dn_Xn[[1]]; names(Dn) = EIDs
 Xn = Dn_Xn[[2]]
-
-A_temp = B_temp = Dn_temp = Xn_temp = Dn_omega_temp = W_temp = list(); 
-
-for(i in 1:length(EIDs_temp)) {
-    A_temp[[i]] = alpha_i_mat[[which(EIDs == EIDs_temp[i])]]
-    B_temp[[i]] = B[[which(EIDs == EIDs_temp[i])]]
-    Dn_temp[[i]] = Dn[[which(EIDs == EIDs_temp[i])]]
-    Xn_temp[[i]] = Xn[[which(EIDs == EIDs_temp[i])]]
-    Dn_omega_temp[[i]] = Dn_omega[[which(EIDs == EIDs_temp[i])]]
-    W_temp[[i]] = W[[which(EIDs == EIDs_temp[i])]]
-}
-
-bleed_indicator_temp = bleed_indicator[Y[,"EID"] %in% EIDs_temp]
-n_cores = 2
-#  ----------------------------------------------------------------------------
 
 # Sampling type ---------------------------------------------------------------
 # samp_type = 1: random selection MH
@@ -155,136 +136,200 @@ n_cores = 2
 # samp_type = 3: sub. likelihood  MH
 # samp_type = 4: Gibbs update
 
-
-args = commandArgs(TRUE)
-samp_type = as.numeric(args[1]) # 1 - 4
-t_pt_length = 5
-
-print(paste0("Sampling scheme: ", samp_type))
-
-# samp_and_p = as.numeric(args[1]) # 1 - 16
-# testing_combo_list = cbind(rep(1:4, 4), rep(2:5, each=4))
-# samp_type = testing_combo_list[samp_and_p, 1]
-# t_pt_length = testing_combo_list[samp_and_p, 2]
-
-it_length = 10000
-post_prob_b = matrix(nrow = it_length, ncol = nrow(Y_temp_big))
-accur_b = matrix(0, nrow = it_length, ncol = length(EIDs_temp) + 1)
-colnames(accur_b) = c('t_diff', paste0('percent_', EIDs_temp))
-
-start_t = Sys.time()
-algorithm_s_time = proc.time()
-for(it in 1:it_length) {
-    if(it %% 10 == 0) {print(it)}
-    
-    if(samp_type == 1) {
-        B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
-                             z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
-                             bleed_indicator_temp, n_cores, t_pt_length, 1)
-    } else if(samp_type == 2) {
-        B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
-                             z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
-                             bleed_indicator_temp, n_cores, t_pt_length, 2)
-    } else if(samp_type == 3) {
-        B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
-                             z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
-                             bleed_indicator_temp, n_cores, t_pt_length, 3)
-    } else {
-        B_Dn = update_b_i_gibbs(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
-                                z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
-                                bleed_indicator_temp, n_cores, t_pt_length)
-    }
-    
-    B_temp = B_Dn[[1]]
-    Dn_temp = B_Dn[[2]]
-    
-    # Posterior probability distribution
-    post_prob_b[it, ] = do.call( 'c', B_temp)
-    
-    # Accuracy and timeliness of the sampling routine
-    algorithm_e_time = proc.time();
-    accur_b[it, 1] = algorithm_e_time[3] - algorithm_s_time[3]
-    
-    for(sub in 2:ncol(accur_b)) {
-        b_ind = which(Y_temp_big[,"EID"] == EIDs_temp[sub-1])
-        accur_b[it, sub] = mean(post_prob_b[it, b_ind] == b_i_true[b_ind])
-    }
-    
-    if(algorithm_e_time[3] - algorithm_s_time[3] > 36000) {
-        # Max time limit is 10 hours
-        print(paste0("Reached maximum time with ", it, " iterations"))
+for(o in 1:4) {
+    for(p in 2:5) {
+        samp_type = o
+        t_pt_length = p
         
-        post_prob_b = post_prob_b[1:it, ]
-        accur_b = accur_b[1:it,]
-        break;
+        print(paste0("Sampling: ", samp_type, ", p = ", t_pt_length))
+        
+        A_temp = B_temp = Dn_temp = Xn_temp = Dn_omega_temp = W_temp = list(); 
+        
+        for(i in 1:length(EIDs_temp)) {
+            A_temp[[i]] = alpha_i_mat[[which(EIDs == EIDs_temp[i])]]
+            B_temp[[i]] = B[[which(EIDs == EIDs_temp[i])]]
+            Dn_temp[[i]] = Dn[[which(EIDs == EIDs_temp[i])]]
+            Xn_temp[[i]] = Xn[[which(EIDs == EIDs_temp[i])]]
+            Dn_omega_temp[[i]] = Dn_omega[[which(EIDs == EIDs_temp[i])]]
+            W_temp[[i]] = W[[which(EIDs == EIDs_temp[i])]]
+        }
+        
+        bleed_indicator_temp = bleed_indicator[Y[,"EID"] %in% EIDs_temp]
+        n_cores = 2
+        #  ----------------------------------------------------------------------------
+        
+        it_length = 10000
+        post_prob_b = matrix(nrow = it_length, ncol = nrow(Y_temp_big))
+        accur_b = matrix(0, nrow = it_length, ncol = length(EIDs_temp) + 1)
+        colnames(accur_b) = c('t_diff', paste0('percent_', EIDs_temp))
+        
+        start_t = Sys.time()
+        algorithm_s_time = proc.time()
+        for(it in 1:it_length) {
+            if(it %% 100 == 0) {print(it)}
+            
+            if(samp_type == 1) {
+                B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
+                                     z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
+                                     bleed_indicator_temp, n_cores, t_pt_length, 1)
+            } else if(samp_type == 2) {
+                B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
+                                     z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
+                                     bleed_indicator_temp, n_cores, t_pt_length, 2)
+            } else if(samp_type == 3) {
+                B_Dn = update_b_i_MH(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
+                                     z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
+                                     bleed_indicator_temp, n_cores, t_pt_length, 3)
+            } else {
+                B_Dn = update_b_i_gibbs(EIDs_temp, par_temp, par_index, A_temp, B_temp, Y_temp_big, 
+                                        z_temp_big, Dn_temp, Xn_temp, Dn_omega_temp, W_temp, 
+                                        bleed_indicator_temp, n_cores, t_pt_length)
+            }
+            
+            B_temp = B_Dn[[1]]
+            Dn_temp = B_Dn[[2]]
+            
+            # Posterior probability distribution
+            post_prob_b[it, ] = do.call( 'c', B_temp)
+            
+            # Accuracy and timeliness of the sampling routine
+            algorithm_e_time = proc.time();
+            accur_b[it, 1] = algorithm_e_time[3] - algorithm_s_time[3]
+            
+            for(sub in 2:ncol(accur_b)) {
+                b_ind = which(Y_temp_big[,"EID"] == EIDs_temp[sub-1])
+                accur_b[it, sub] = mean(post_prob_b[it, b_ind] == b_i_true[b_ind])
+            }
+            
+            if(algorithm_e_time[3] - algorithm_s_time[3] > 36000) {
+                # Max time limit is 10 hours
+                print(paste0("Reached maximum time with ", it, " iterations"))
+                
+                post_prob_b = post_prob_b[1:it, ]
+                accur_b = accur_b[1:it,]
+                break;
+            }
+        }
+        end_t = Sys.time()
+        print(paste0("Total MCMC time: ", end_t - start_t))
+        
+        sampling_out = list(post_prob_b = post_prob_b, accur_b = accur_b)
+        save(sampling_out, 
+             file = paste0('Model_out/sampling_out_',samp_type,'_',t_pt_length, 
+                           '_rep', e_num, '.rda'))
     }
 }
-end_t = Sys.time()
-print(paste0("Total MCMC time: ", end_t - start_t))
-
-sampling_out = list(post_prob_b = post_prob_b, accur_b = accur_b)
-save(sampling_out, 
-     file = paste0('Model_out/sampling_out_',samp_type,'_',t_pt_length,'.rda'))
 
 
-# Plot the results
-pdf(paste0('Plots/sampling_out_',samp_type,'_',t_pt_length,'.pdf'))
-par(mfrow = c(2,1))
-for(i in 1:length(EIDs_temp)) {
-    b_ind = which(Y_temp_big[,"EID"] == EIDs_temp[i])
-    pb = barplot(rbind(colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 1),
-                       colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 2),
-                       colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 3),
-                       colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 4),
-                       colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 5)), 
-                 col=c( 'dodgerblue', 'firebrick1', 'yellow2', 'green', 'darkgray'), 
-                 xlab='time', space=0, col.main='black', border=NA, axes = F, plot = F)
-    
-    barplot(rbind(colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 1),
-                  colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 2),
-                  colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 3),
-                  colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 4),
-                  colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 5)), 
-            col=c( 'dodgerblue', 'firebrick1', 'yellow2', 'green', 'darkgray'), 
-            xlab='true states', space=0, col.main='black', border=NA,
-            xlim=range(pb) + c(-0.5,0.5), main = EIDs_temp[i]) 
-    grid( nx=20, NULL, col='white')
-    axis( side=1, at=1:length(b_ind)-0.5, labels = b_i_true[b_ind], cex.axis=0.3)
-}
-
-plot(x = accur_b[,1], y = accur_b[,2], type = 'l', ylim=c(0,1), lwd=2,
-     xlab = 'compute time (sec.)', ylab = 'state accuracy (at each it.)')
-for(j in 2:length(EIDs_temp)) {
-    lines(x = accur_b[,1], y = accur_b[,j+1], lwd = 2, col = j)
-}
-dev.off()
-
-
+# # Plot the results
+# pdf(paste0('Plots/sampling_out_',samp_type,'_',t_pt_length,'.pdf'))
+# par(mfrow = c(2,1))
+# for(i in 1:length(EIDs_temp)) {
+#     b_ind = which(Y_temp_big[,"EID"] == EIDs_temp[i])
+#     pb = barplot(rbind(colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 1),
+#                        colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 2),
+#                        colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 3),
+#                        colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 4),
+#                        colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 5)), 
+#                  col=c( 'dodgerblue', 'firebrick1', 'yellow2', 'green', 'darkgray'), 
+#                  xlab='time', space=0, col.main='black', border=NA, axes = F, plot = F)
+#     
+#     barplot(rbind(colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 1),
+#                   colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 2),
+#                   colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 3),
+#                   colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 4),
+#                   colMeans(post_prob_b[1:nrow(post_prob_b), b_ind] == 5)), 
+#             col=c( 'dodgerblue', 'firebrick1', 'yellow2', 'green', 'darkgray'), 
+#             xlab='true states', space=0, col.main='black', border=NA,
+#             xlim=range(pb) + c(-0.5,0.5), main = EIDs_temp[i]) 
+#     grid( nx=20, NULL, col='white')
+#     axis( side=1, at=1:length(b_ind)-0.5, labels = b_i_true[b_ind], cex.axis=0.3)
+# }
+# 
+# plot(x = accur_b[,1], y = accur_b[,2], type = 'l', ylim=c(0,1), lwd=2,
+#      xlab = 'compute time (sec.)', ylab = 'state accuracy (at each it.)')
+# for(j in 2:length(EIDs_temp)) {
+#     lines(x = accur_b[,1], y = accur_b[,j+1], lwd = 2, col = j)
+# }
+# dev.off()
+# 
+# 
 # # Look at accuracy across all sampling schemes
 # EIDs_temp = c(194350, 234375, 259825, 288775, 747775)
 # pdf(paste0('Plots/sampling_out_summary.pdf'))
 # par(mfrow = c(4,1), mar=c(2,4,2,4))
-# for(t in 2:4) {
+# for(p in 2:5) {
 #     for(s in 1:4) {
-#         load(paste0('Model_out/sampling_out_',s,'_',t,'.rda'))
+#         load(paste0('Model_out/sampling_out_',s,'_',p,'.rda'))
 #         accur_b = sampling_out$accur_b; rm(sampling_out)
 #         plot(x = accur_b[,1], y = accur_b[,2], type = 'l', ylim=c(0,1), lwd=2,
-#              xlab = 'compute time (sec.)', ylab = 'state accuracy (at each it.)')
+#              xlab = 'compute time (sec.)', ylab = 'state accuracy (at each it.)',
+#              col = 1)
 #         for(j in 2:length(EIDs_temp)) {
-#             lines(x = accur_b[,1], y = accur_b[,j+1], lwd = 1, lty = j)
+#             lines(x = accur_b[,1], y = accur_b[,j+1], lwd = 1, lty = j, col = j+1)
 #         }
 #         avg_correct = rowMeans(accur_b[,2:ncol(accur_b)])
 #         lines(x = accur_b[,1], y = avg_correct, lwd = 2, col = 'red')
 #         time_convg = accur_b[min(which(avg_correct == max(avg_correct))),1]
 #         abline(v = time_convg, col = 'blue')
-#         # axis(1, at=time_convg, labels=c(time_convg), col = 'blue') 
-#         mtext(side=3, line=0, at=-0.07, adj=0, cex=0.7, paste0('Sampling opt = ', s, ', p = ', t, 
-#                                                                ' (',round(time_convg,digits=2), ', ', 
+#         
+#         convg_times = accur_b[apply(accur_b[,2:ncol(accur_b)], 2, which.max), 1]
+#         mean_convg = mean(convg_times)
+#         abline(v = mean_convg, col = 'orange')
+#         # axis(1, at=time_convg, labels=c(time_convg), col = 'blue')
+#         mtext(side=3, line=0, at=-0.07, adj=0, cex=0.7, paste0('Sampling opt = ', s, ', p = ', p,
+#                                                                ' (',round(time_convg,digits=2), ', ',
 #                                                                round(max(avg_correct),digits=2), ')'))
+#         if(s==1) {
+#             legend(x = "bottomright", legend = c("avg.", EIDs_temp), 
+#                    col = c(2,1,3:(length(EIDs_temp)+1)),lwd = 2)  
+#         }
 #     }
 # }
 # dev.off()
 # 
 # # Plot a grid to compare converge time to max correctness (and compare to time per step)
+# mean_convg_mat1 = mean_convg_mat2 = max_correct_mat = num_steps = matrix(nrow=4,ncol=4)
+# for(p in 2:5) {
+#     for(s in 1:4) {
+#         load(paste0('Model_out/sampling_out_',s,'_',p,'.rda'))
+#         accur_b = sampling_out$accur_b; rm(sampling_out)
+#         avg_correct = rowMeans(accur_b[,2:ncol(accur_b)])
+#         convg_times = accur_b[apply(accur_b[,2:ncol(accur_b)], 2, which.max), 1]
+#         
+#         mean_convg_t1 = mean(convg_times)
+#         mean_convg_t2 = as.numeric(accur_b[min(which(avg_correct == max(avg_correct))),1])
+#         mean_max_correct = mean(apply(accur_b[,2:ncol(accur_b)], 2, max))
+#         
+#         mean_convg_mat1[s,p-1] = mean_convg_t1
+#         mean_convg_mat2[s,p-1] = mean_convg_t2
+#         max_correct_mat[s,p-1] = mean_max_correct
+#         num_steps[s,p-1] = which.max(accur_b[,1] > mean_convg_t1)
+#     }
+# }
+# 
+# rownames(mean_convg_mat1) = rownames(mean_convg_mat2) = 
+#     rownames(max_correct_mat) = rownames(num_steps) = paste0("s = ",1:4)
+# colnames(mean_convg_mat1) = colnames(mean_convg_mat2) = 
+#     colnames(max_correct_mat) = colnames(num_steps) = paste0("p = ",2:5)
+# 
+# print("Convergence time")
+# print(mean_convg_mat1)
+# print("Number of steps")
+# print(num_steps)
+# print("Average max correctness")
+# print(max_correct_mat)
 
+# pt_label = c(paste0("(opt=", 1:4, ", p=", rep(2:5, each = 4), ")"))
+# 
+# pdf(paste0('Plots/sampling_out_summary2.pdf'))
+# par(mfrow = c(2,1))
+# plot(x = c(mean_convg_mat1), y = c(max_correct_mat), main = ' ', ylab = 'max percent correct',
+#      xlab = 'avg. convergence time', xlim = c(0, max(c(mean_convg_mat1))), ylim = c(0,1))
+# # text(x = c(mean_convg_mat1), y = c(max_correct_mat)-0.1, labels=pt_label)
+# 
+# plot(x = c(mean_convg_mat2), y = c(max_correct_mat), main = ' ', ylab = 'max percent correct',
+#      xlab = 'avg. convergence time', xlim = c(0, max(c(mean_convg_mat1))), ylim = c(0,1))
+# # text(x = c(mean_convg_mat2), y = c(max_correct_mat)-0.1, labels=pt_label)
+# dev.off()
 
